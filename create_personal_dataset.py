3fa02ccf7bcb07cbed75b99085bcf6f90158dd1e
BlankLudicrous <sandeepl7310@gmail.com>
2026-01-26 23:02:43 +0530

    Update create_personal_dataset.py

--- create_personal_dataset.py
+++ create_personal_dataset.py
@@ -10,3 +10,13 @@ with open("student_details.csv","w+",newline="") as p:
     writer=csv.writer(p)
     writer.writerow(["name","age","marks"])
     writer.writerows(students)
+with open("student_details.csv","r") as p:
+    data=csv.reader(p)
+    next(data)
+    for row in data:
+        name=row[0]
+        age=int(row[1])
+        marks=int(row[2]) 
+        print(f"name {name}"
+              f"age {age}"
+              f"marks {marks}")
